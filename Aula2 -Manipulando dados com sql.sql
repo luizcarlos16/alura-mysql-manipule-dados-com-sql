6c0741b8fc4bc89dba5da7f6b36172aa1e93a2c7
Maison <maisongalvao144@gmail.com>
2019-12-16 21:32:36 -0400

    Aula02 - Manipulando o banco de dados

---
+++ Aula2 -Manipulando dados com sql.sql
@@ -0,0 +1,11 @@
+-- Criação do banco de dados
+CREATE DATABASE SUCOS;
+
+-- Apagar um banco de dados
+DROP DATABASE sucos3;
+
+-- Para acessar por linha de comando
+mysql -h localhost -u root -p;
+
+-- selecionar um banco existente
+USE nomedobanco;
